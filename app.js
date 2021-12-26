40c48233be067d780e0ba8f132c2ecb398ea5ce7
Mohamed Gallab <mohamed.w.gallab@gmail.com>
2021-12-26 18:46:24 +0200

    testing cookies

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ app.use(session({
   store: new MongoStore({
     mongoUrl: url,
     autoRemove: 'native',
-    ttl: 5 * 60  // live for 5 mins
+    ttl: 20  // live for 5 mins
   })
 }));
 
@@ -205,7 +205,7 @@ app.post('/register', (req, res) => {
 
 // catch trying to go to a page directly
 app.use(function(req, res, next) {
-  if(!req.session.username) {       // requiring a valid access token
+  if(!req.session.username || !req.session) {       // requiring a valid access token
       res.redirect('/login');
   } else {
       next();
